2e06487165ed74ea3ab6c7423a6f173630bcb31c
imakyo97 <kyoheishogun.1997@gmail.com>
2024-03-14 16:27:36 +0900

    react-draggableでドラックできるコンポーネントを作成

--- next-admin/src/components/react_admin/project_asigns/ProjectAsign.tsx
+++ next-admin/src/components/react_admin/project_asigns/ProjectAsign.tsx
@@ -1,8 +1,9 @@
 "use client";  // クライアントコンポーネント
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import DeleteClient from "@/components/apollo_client/clients/DeleteClient";
 import { Title } from "react-admin";
+import Draggable from 'react-draggable';
+import { Card } from "@mui/material"
 
 const client = new ApolloClient({
     uri: "http://localhost:8000/graphql",
@@ -14,7 +15,9 @@ export const ProjectAsign = () => {
     return (
         <ApolloProvider client={client}>
             <Title title={"プロジェクトアサイン"} />
-            
+            <Draggable>
+                <Card variant="outlined">動く文字</Card>
+            </Draggable>
         </ApolloProvider>
     )
 }
