94daf193828b38fc6e9f2e49adfb083f807432f3
imakyo97 <kyoheishogun.1997@gmail.com>
2024-03-17 23:32:06 +0900

    restのdataProviderを作成

--- next-admin/src/components/react_admin/AdminApp.tsx
+++ next-admin/src/components/react_admin/AdminApp.tsx
@@ -1,29 +1,18 @@
 "use client"; 
 
 import React from 'react';
-import { Component } from 'react';
-import buildGraphQLProvider from 'ra-data-graphql-simple';
-import { Admin, DataProvider, Resource, CustomRoutes } from 'react-admin';
+import { Admin, Resource, CustomRoutes } from 'react-admin';
 import { Route } from "react-router-dom";
 import clients from './clients'
 import { MainLayout } from './MainLayout';
 import { ProjectAsign } from "./project_asigns"
+import dataProvider from '@/app/dataProvider';
 
 
 const AdminApp = () => {
-    const [dataProvider, setDataProvider] = React.useState<DataProvider | null>(null)
-    React.useEffect(() => {
-        buildGraphQLProvider({ clientOptions: { uri: 'http://localhost:8000/graphql' } })
-            .then(graphQlDataProvider => setDataProvider(graphQlDataProvider));
-    }, []);
-
-    if (!dataProvider) {
-        return <div>Loading </div>;
-    }
-
     return (
         <Admin dataProvider= { dataProvider } layout={ MainLayout }>
-            <Resource name="Client" {...clients} />
+            <Resource name="clients" {...clients} />
             <CustomRoutes>
                 <Route path="/project_asigns" element={<ProjectAsign />} />
             </CustomRoutes>
