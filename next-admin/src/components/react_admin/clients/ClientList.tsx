4809727f7423f206835d89156bbd9fc16613ded2
imakyo97 <kyoheishogun.1997@gmail.com>
2024-03-18 22:49:28 +0900

    ClientListコンポーネントを修正

--- next-admin/src/components/react_admin/clients/ClientList.tsx
+++ next-admin/src/components/react_admin/clients/ClientList.tsx
@@ -1,23 +1,46 @@
 import {
     List,
-    Datagrid,
     EditButton,
     TextField,
     DateField,
-    TextInput
+    TextInput,
+    SearchInput,
+    TopToolbar,
+    SelectColumnsButton,
+    FilterButton,
+    CreateButton,
+    ExportButton,
+    DatagridConfigurable,
+    WrapperField,
+    Pagination,
 } from "react-admin";
 
+const ListActions = () => (
+  <TopToolbar>
+      <SelectColumnsButton />
+      <FilterButton/>
+      <CreateButton/>
+      <ExportButton/>
+  </TopToolbar>
+);
+
 const clientFilters = [
-  <TextInput source="name" label="Search" alwaysOn />,
+  <TextInput label="Search" source="name" alwaysOn resettable/>,
+  <TextInput label="created_at" source="created_at" type="date" alwaysOn resettable/>,
+  <SearchInput source="updated_at" alwaysOn/>,
 ];
 
+const ClientPagination = () => <Pagination rowsPerPageOptions={[10, 25, 50, 100]} />;
+
 export const ClientList = (props: any) => (
-    <List filters={clientFilters}>
-      <Datagrid>
+    <List actions={<ListActions/>} filters={clientFilters} pagination={<ClientPagination/>}>
+      <DatagridConfigurable>
         <TextField source="name" label="名前"/>
         <DateField source="created_at" label="登録日時"/>
         <DateField source="updated_at" lable="更新日時"/>
-        <EditButton/>
-      </Datagrid>
+        <WrapperField label="Actions">
+          <EditButton />
+        </WrapperField>
+      </DatagridConfigurable>
     </List>
 );
